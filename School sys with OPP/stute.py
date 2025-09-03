52f5e9f4b3f19ad94405d376ffc7ab15987ebf87
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-09-03 17:11:26 +0600

    Updated person, Student, and Teacher classes

--- School sys with OPP/stute.py
+++ School sys with OPP/stute.py
@@ -1,33 +1,38 @@
-class person:#parent class
-    def __init__(self,*arges,**kwargs): #constructor
-        self.name=kwargs.get("name",arges[0] if len(arges)>0 else None) #instance variable ja hoy argument theke na hoy keyword the data nibe ,,,aitar ekta problem holo hoy data dictionary akare dau na hole position thik rakhao
-        self.phone=kwargs.get("phone",arges[1] if len(arges)>1 else None)
-        
+class person: #parent class
+    def __init__(self,name,phone): #constructor
+        self.name=name 
+        self.phone=phone
 
 
-        def show(self): #function
-            pass
+    def show(self): #function
+        pass
 
 
 class Student(person): # child class
-    def __init__(self,*arges,**kwargs):
-        super().__init__(*arges,**kwargs) #aita use kore parent er variable r call kora lage na
-        self.__student_id=kwargs.get("student_id",arges[2] if len(arges)>2 else None)  #instance variable ja hoy argument theke na hoy keyword the data nibe ,,,aitar ekta problem holo hoy data dictionary akare dau na hole position thik rakhao
-        self.student_class=kwargs.get("student_class",arges[3] if len(arges)>3 else None)
-        self.student_class_roll=kwargs.get("student_class_roll",arges[4] if len(arges)>4 else None)
-        self.__grade=kwargs.get("grade",arges[5] if len(arges)>5 else None)
+    def __init__(self,name,phone,student_id,student_class,student_class_roll,grade):
+        super().__init__(name,phone) #aita use kore parent er variable r call kora lage na
+        self.__student_id=student_id  #instance variable ja hoy argument theke na hoy keyword the data nibe ,,,aitar ekta problem holo hoy data dictionary akare dau na hole position thik rakhao
+        self.student_class=student_class
+        self.student_class_roll=student_class_roll
+        self.__grade=grade
 
     def show(self):
-        return f"Student Name:{self.name},Student ID:{self.__student_id}, Class:{self.student_class,}Class Roll:{self.student_class_roll},Grade:{self.__grade}"
+        return f"Student Name:{self.name},phone:{self.phone}Student ID:{self.__student_id}, Class:{self.student_class},Class Roll:{self.student_class_roll},Grade:{self.__grade}"
 
 
 class Teacher(person): #chils class
-    def __init__(self,*arges,**kwargs):
-        super().__init__(*arges,**kwargs)   #aita use kore parent er variable r call kora lage na
-        self.__teacher_id=kwargs.get("teacher_id",arges[2] if len(arges)>2 else None)  #instance variable ja hoy argument theke na hoy keyword the data nibe ,,,aitar ekta problem holo hoy data dictionary akare dau na hole position thik rakhao
-        self.subject=kwargs.get("subject",arges[3] if len(arges)>3 else None)
+    def __init__(self,name,phone,teacher_id,subject):
+        super().__init__(name,phone)   #aita use kore parent er variable r call kora lage na
+        self.__teacher_id=teacher_id  #instance variable ja hoy argument theke na hoy keyword the data nibe ,,,aitar ekta problem holo hoy data dictionary akare dau na hole position thik rakhao
+        self.subject=subject
 
     def show(self):
         return f"Teacher Name:{self.name},Teacher ID:{self.__teacher_id},Subject:{self.subject}"
+    
+
+
+s1=Student("Abid","017222","ST001","6","30","A+") 
+print(s1.show())   
         
-        
+t1=Teacher("Karim","017333","T001","Math")   
+print(t1.show())     
---
+++ School sys with OPP/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+c1=school.add_course(
+    course_name="Bangla",
+    course_teacher="anik"
+
+)
+print ("All subject")
+print(school.show_all_course())
+                      
